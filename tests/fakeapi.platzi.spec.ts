938d2b48f28bd83a745aee01f465749d1aa903a7
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-06-21 15:32:34 +0300

    Add tests for product CRUD operations

--- tests/fakeapi.platzi.spec.ts
+++ tests/fakeapi.platzi.spec.ts
@@ -1,6 +1,171 @@
 import { test, expect } from "@playwright/test";
 
-test("get products", async ({ request }) => {
-  const response = await request.get('https://api.escuelajs.co/api/v1/products', { failOnStatusCode: true });
+function generateRandomProductTitle(): string {
+  const minLength = 3;
+  const maxLength = 20;
+  const length =
+    Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+  let title = "";
+
+  for (let i = 0; i < length; i++) {
+    title += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return title.trim();
+}
+
+test("Get products", async ({ request }) => {
+  const response = await request.get(
+    "https://api.escuelajs.co/api/v1/products",
+    { failOnStatusCode: true },
+  );
   expect(response.status()).toBe(200);
+});
+
+test("Create product", async ({ request }) => {
+  let title = generateRandomProductTitle();
+  let description = generateRandomProductTitle();
+
+  const response = await request.post(
+    "https://api.escuelajs.co/api/v1/products",
+    {
+      data: {
+        title: title,
+        slug: "handmade-fresh-table",
+        price: 123,
+        description: description,
+        categoryId: 1,
+        images: ["https://placehold.co/600x400"],
+      },
+    },
+  );
+  const json = await response.json();
+
+  const productId = json.id;
+  expect(response.status()).toBe(201);
+  expect(response.statusText()).toMatch("Created");
+  expect(response.headers()["content-type"]).toContain("application/json");
+  expect(json).toHaveProperty("id");
+  expect(json).toHaveProperty("price", 123);
+  expect(json.title).toBe(title);
+  expect(json.description).toEqual(description);
+
+  // Check product exists
+  const responseGetById = await request.get(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    { failOnStatusCode: true },
+  );
+  expect(responseGetById.status()).toBe(200);
+  expect(json).toHaveProperty("id", productId);
+  
+
+  // Clean Up
+  const responseDel = await request.delete(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    { failOnStatusCode: true },
+  );
+  expect(response).toBeOK(); // only for 2xx;
+  expect(responseDel.status()).toBe(200)
+});
+
+test("Update product", async ({ request }) => {
+  // Create product
+  let title = generateRandomProductTitle();
+  let description = generateRandomProductTitle();
+
+  const responsePost = await request.post(
+    "https://api.escuelajs.co/api/v1/products",
+    {
+      failOnStatusCode: true,
+      data: {
+        title: title,
+        slug: "handmade-fresh-table",
+        price: 123,
+        description: description,
+        categoryId: 1,
+        images: ["https://placehold.co/600x400"]
+      }
+    }
+  );
+  const jsonPost = await responsePost.json();
+
+  const productId = jsonPost.id;
+  expect(responsePost.status()).toBe(201);
+  expect(responsePost.statusText()).toMatch("Created");
+
+  // Update product
+  let updatedTitle = generateRandomProductTitle();
+  let updatedDescription = generateRandomProductTitle();
+
+  const responsePut = await request.put(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    {
+      failOnStatusCode: true,
+      data: {
+        title: updatedTitle,
+        slug: "handmade-fresh-table",
+        price: 123,
+        description: updatedDescription,
+        categoryId: 1,
+        images: ["https://placehold.co/600x400"],
+      }
+    }
+  );
+  let jsonPut = await responsePut.json();
+
+  expect(responsePut.status()).toBe(200);
+  expect(jsonPut.title).toBe(updatedTitle);
+  expect(jsonPut.description).toEqual(updatedDescription);
+
+  // Clean Up
+  const responseDel = await request.delete(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    { failOnStatusCode: true },
+  );
+  expect(responseDel).toBeOK(); // only for 2xx;
+  expect(responseDel.status()).toBe(200)
+});
+
+test("Delete product", async ({ request }) => {
+  // Create product
+  let title = generateRandomProductTitle();
+
+  const responsePost = await request.post(
+    "https://api.escuelajs.co/api/v1/products",
+    {
+      failOnStatusCode: true,
+      data: {
+        title: title,
+        slug: "handmade-fresh-table",
+        price: 123,
+        description: generateRandomProductTitle(),
+        categoryId: 1,
+        images: ["https://placehold.co/600x400"]
+      }
+    }
+  );
+  const jsonPost = await responsePost.json();
+  const productId = jsonPost.id;
+  expect(responsePost.status()).toBe(201);
+  expect(responsePost.statusText()).toMatch("Created");
+
+  // Delete product
+  const responseDel = await request.delete(
+    `https://api.escuelajs.co/api/v1/products/${productId}`,
+    { failOnStatusCode: true },
+  );
+  expect(responseDel).toBeOK(); // only for 2xx;
+  expect(responseDel.status()).toBe(200)
+
+  // Check product NOT exists
+  const responseGetById = await request.get(
+    `https://api.escuelajs.co/api/v1/products/${productId}`
+  );
+  const jsonGet = await responseGetById.json();
+  // expect(responseGetById.status()).toBe(404);
+  expect(jsonGet.name).toBe("EntityNotFoundError")
+
 });
