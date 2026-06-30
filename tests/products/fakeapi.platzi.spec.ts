395307fa1c1842032f7833d668a28c49ba91e0ab
Vitalii Ostrovyi <vostrovyi@gmail.com>
2026-06-30 18:56:20 +0300

    Add pagination check and enhance product creation and deletion tests

--- tests/products/fakeapi.platzi.spec.ts
+++ tests/products/fakeapi.platzi.spec.ts
@@ -34,12 +34,24 @@ test.describe("GET products", { tag: ["@getProduct"] }, () => {
       expect(response.status()).toBe(200);
     });
   });
+
+  test("Check pagination 10", async ({ request }) => {
+    await test.step("Check pagination contains 10 products", async () => {
+      const response = await request.get(`/api/v1/products`, {
+        params: {
+          offset: 0,
+          limit: 10,
+        },
+      });
+
+      const json = await response.json();
+      expect(response.status()).toBe(200);
+      expect(json).toHaveLength(10);
+    });
+  });
 });
 
-test.describe(
-  "Products tests with Clean Up(?)",
-  { tag: ["@products", "@cleanUp"] },
-  () => {
+test.describe("Products tests with Clean Up(?)", {tag: ["@products", "@cleanUp"]}, () => {
     let productIdAndSlug;
     let uniqueTitle: string;
 
@@ -182,209 +194,152 @@ test.describe("Create products", { tag: ["@products", "@cleanUp"] }, () => {
   });
 
   test("Create product", async ({ request }) => {
-    const response = await request.post("/api/v1/products", {
-      data: {
-        title: uniqueTitle,
-        slug: "handmade-fresh-table",
-        price: 123,
-        description: uniqueDescription,
-        categoryId: 1,
-        images: ["https://placehold.co/600x400"],
-      },
+    await test.step("Create product", async () => {
+      const response = await request.post("/api/v1/products", {
+        data: {
+          title: uniqueTitle,
+          slug: "handmade-fresh-table",
+          price: 123,
+          description: uniqueDescription,
+          categoryId: 1,
+          images: ["https://placehold.co/600x400"],
+        },
+      });
+      const json = await response.json();
+
+      productId = json.id;
+      //! ASSERT
+      expect(response.status()).toBe(201);
+      expect(response.statusText()).toMatch("Created");
+      expect(response.headers()["content-type"]).toContain("application/json");
+      expect(json).toHaveProperty("id");
+      expect(json).toHaveProperty("price", 123);
+      expect(json.title).toBe(uniqueTitle);
+      expect(json.description).toEqual(uniqueDescription);
     });
-    const json = await response.json();
 
-    productId = json.id;
-    //! ASSERT
-    expect(response.status()).toBe(201);
-    expect(response.statusText()).toMatch("Created");
-    expect(response.headers()["content-type"]).toContain("application/json");
-    expect(json).toHaveProperty("id");
-    expect(json).toHaveProperty("price", 123);
-    expect(json.title).toBe(uniqueTitle);
-    expect(json.description).toEqual(uniqueDescription);
+    await test.step("Check product exists", async () => {
+      const responseGetById = await request.get(
+        `/api/v1/products/${productId}`,
+        {
+          failOnStatusCode: true,
+        },
+      );
+      const jsonGetById = await responseGetById.json();
 
-    // Check product exists
-    const responseGetById = await request.get(`/api/v1/products/${productId}`, {
-      failOnStatusCode: true,
+      expect(responseGetById.status()).toBe(200);
+      expect(jsonGetById).toHaveProperty("id", productId);
     });
-    const jsonGetById = await responseGetById.json();
-
-    expect(responseGetById.status()).toBe(200);
-    expect(jsonGetById).toHaveProperty("id", productId);
   });
 });
 
-// test("Delete product", async ({ request }) => {
-//   // Create product
-//   let title = generateRandomProductTitle();
+test.describe("Delete products", { tag: ["@products"] }, () => {
+  let productIdAndSlug;
+  let uniqueTitle: string;
 
-//   const responsePost = await request.post("/api/v1/products", {
-//     failOnStatusCode: true,
-//     data: {
-//       title: title,
-//       slug: "handmade-fresh-table",
-//       price: 123,
-//       description: generateRandomProductTitle(),
-//       categoryId: 1,
-//       images: ["https://placehold.co/600x400"],
-//     },
-//   });
-//   const jsonPost = await responsePost.json();
-//   const productId = jsonPost.id;
-//   expect(responsePost.status()).toBe(201);
-//   expect(responsePost.statusText()).toMatch("Created");
+  test.beforeEach(async ({ request }) => {
+    //! ARRANGE
+    uniqueTitle = await test.step("Generate unique title", async () => {
+      return generateRandomProductTitle();
+    });
 
-//   // Delete product
-//   const responseDel = await request.delete(`/api/v1/products/${productId}`, {
-//     failOnStatusCode: true,
-//   });
-//   expect(responseDel).toBeOK(); // only for 2xx;
-//   expect(responseDel.status()).toBe(200);
+    const uniqueDescription =
+      await test.step("Generate unique description", async () => {
+        return generateRandomProductTitle();
+      });
 
-//   // Check product NOT exists
-//   const responseGetById = await request.get(`/api/v1/products/${productId}`);
-//   const jsonGet = await responseGetById.json();
-//   // expect(responseGetById.status()).toBe(404);
-//   expect(jsonGet.name).toBe("EntityNotFoundError");
-// });
+    const responseCreate =
+      await test.step(`Create product with title: ${uniqueTitle}`, async () => {
+        const response = await request.post("/api/v1/products/", {
+          data: {
+            title: uniqueTitle,
+            price: 10,
+            description: uniqueDescription,
+            categoryId: 1,
+            images: ["https://placehold.co/600x400"],
+          },
+          failOnStatusCode: true,
+        });
 
-// test("Get a single product by id", async ({ request }) => {
-//   // Create product
-//   let title = generateRandomProductTitle();
+        return response;
+      });
 
-//   const responsePost = await request.post("/api/v1/products", {
-//     failOnStatusCode: true,
-//     data: {
-//       title: title,
-//       slug: "handmade-fresh-table",
-//       price: 123,
-//       description: generateRandomProductTitle(),
-//       categoryId: 1,
-//       images: ["https://placehold.co/600x400"],
-//     },
-//   });
-//   const jsonPost = await responsePost.json();
-//   const productId = jsonPost.id;
-//   expect(responsePost.status()).toBe(201);
-//   expect(responsePost.statusText()).toMatch("Created");
+    let jsonCreate = await responseCreate.json();
+    const productId = jsonCreate["id"];
+    const productSlug = jsonCreate["slug"];
 
-//   // Get a single product by id
-//   const responseGetById = await request.get(`/api/v1/products/${productId}`, {
-//     failOnStatusCode: true,
-//   });
-//   const jsonGetById = await responseGetById.json();
+    productIdAndSlug = { productId, productSlug };
+  });
 
-//   expect(responseGetById.status()).toBe(200);
-//   expect(jsonGetById).toHaveProperty("id", productId);
-//   expect(jsonGetById).toHaveProperty("title", title);
+  test("Delete product", async ({ request }) => {
+    await test.step("Delete product by id", async () => {
+      const responseDel = await request.delete(
+        `/api/v1/products/${productIdAndSlug!.productId}`,
+        {
+          failOnStatusCode: true,
+        },
+      );
+      expect(responseDel).toBeOK(); // only for 2xx;
+      expect(responseDel.status()).toBe(200);
+    });
 
-//   // Clean Up
-//   const responseDel = await request.delete(`/api/v1/products/${productId}`, {
-//     failOnStatusCode: true,
-//   });
-//   expect(responseDel).toBeOK(); // only for 2xx;
-//   expect(responseDel.status()).toBe(200);
-// });
+    await test.step("Check product by Id is NOT exists", async () => {
+      const responseGetById = await request.get(
+        `/api/v1/products/${productIdAndSlug!.productId}`,
+      );
+      const jsonGet = await responseGetById.json();
+      // expect(responseGetById.status()).toBe(404);
+      expect(jsonGet.name).toBe("EntityNotFoundError");
+    });
+  });
+});
 
-// test("Get a single product by slug", async ({ request }) => {
-//   // Create product
-//   let title = generateRandomProductTitle();
-//   let slug = title.toLocaleLowerCase();
+test.describe("Get products related by id", { tag: ["@products"] }, () => {
+  test("Get products related by id", async ({ request }) => {
+    const objects: any[] = [];
+    const productIds: number[] = [];
 
-//   const responsePost = await request.post("/api/v1/products", {
-//     failOnStatusCode: true,
-//     data: {
-//       title: title,
-//       slug: title,
-//       price: 123,
-//       description: generateRandomProductTitle(),
-//       categoryId: 1,
-//       images: ["https://placehold.co/600x400"],
-//     },
-//   });
-//   const jsonPost = await responsePost.json();
-//   const productId = jsonPost.id;
-//   expect(responsePost.status()).toBe(201);
-//   expect(responsePost.statusText()).toMatch("Created");
-//   expect(jsonPost.slug).toBe(slug);
+    await test.step("Create 3 products in same category #3", async () => {
+      for (let i = 0; i < 3; i++) {
+        const responsePost = await request.post("/api/v1/products", {
+          data: {
+            title: generateRandomProductTitle(),
+            slug: "handmade-fresh-table",
+            price: 123,
+            description: generateRandomProductTitle(),
+            categoryId: 3,
+            images: ["https://placehold.co/600x400"],
+          },
+        });
 
-//   // Get a single product by slug
-//   const responseGetBySlug = await request.get(
-//     `/api/v1/products/slug/${slug}`,
-//   );
-//   const jsonGetBySlug = await responseGetBySlug.json();
-//   expect(responseGetBySlug.status()).toBe(200);
-//   expect(jsonGetBySlug).toHaveProperty("id", productId);
-//   expect(jsonGetBySlug).toHaveProperty("title", title);
-//   expect(jsonGetBySlug).toHaveProperty("slug", slug);
+        const jsonPost = await responsePost.json();
+        const id = jsonPost.id;
+        objects.push(jsonPost);
+        productIds.push(id);
+      }
+    });
 
-//   // Clean Up
-//   const responseDel = await request.delete(`/api/v1/products/${productId}`, {
-//     failOnStatusCode: true,
-//   });
-//   expect(responseDel).toBeOK(); // only for 2xx;
-//   expect(responseDel.status()).toBe(200);
-// });
+    await test.step("Get products related by id", async () => {
+      const productId = productIds[0];
+      const responseGet = await request.get(
+        `/api/v1/products/${productId}/related`,
+      );
+      expect(responseGet.status()).toBe(200);
+      const related = await responseGet.json();
+      expect(Array.isArray(related)).toBeTruthy();
+      for (const product of related) {
+        expect(product.category.id).toBe(3);
+      }
+      const relatedIds = related.map((p: any) => p.id);
+      expect(relatedIds).not.toContain(productId);
+    });
 
-// test("Check pagination 10", async ({ request }) => {
-//   const response = await request.get(`/api/v1/products`, {
-//     params: {
-//       offset: 0,
-//       limit: 10,
-//     },
-//   });
-
-//   const json = await response.json();
-//   expect(response.status()).toBe(200);
-//   expect(json).toHaveLength(10);
-// });
-
-// test("Get products related by id", async ({ request }) => {
-//   // Create 3 products in same category #3
-
-//   const objects = [];
-//   const productIds = [];
-//   for (let i = 0; i < 3; i++) {
-//     const responsePost = await request.post("/api/v1/products", {
-//       data: {
-//         title: generateRandomProductTitle(),
-//         slug: "handmade-fresh-table",
-//         price: 123,
-//         description: generateRandomProductTitle(),
-//         categoryId: 3,
-//         images: ["https://placehold.co/600x400"],
-//       },
-//     });
-
-//     const jsonPost = await responsePost.json();
-//     const id = jsonPost.id;
-//     objects.push(jsonPost);
-//     productIds.push(id);
-//   }
-
-//   // Get products related by id
-//   const productId = productIds[0];
-//   const responseGet = await request.get(
-//     `/api/v1/products/${productId}/related`,
-//   );
-//   expect(responseGet.status()).toBe(200);
-//   const related = await responseGet.json();
-//   expect(Array.isArray(related)).toBeTruthy();
-//   for (const product of related) {
-//     expect(product.category.id).toBe(3);
-//   }
-//   const relatedIds = related.map((p: any) => p.id);
-//   expect(relatedIds).not.toContain(productId);
-
-//   // Clean Up
-//   for (const id of productIds) {
-//     await request.delete(`/api/v1/products/${id}`, {
-//       failOnStatusCode: true,
-//     });
-//   }
-// });
-
-// await test.step('User executes POST request on endpoint', async () => {
-
-//   });
+    await test.step("Clean Up", async () => {
+      for (const id of productIds) {
+        await request.delete(`/api/v1/products/${id}`, {
+          failOnStatusCode: true,
+        });
+      }
+    });
+  });
+});
